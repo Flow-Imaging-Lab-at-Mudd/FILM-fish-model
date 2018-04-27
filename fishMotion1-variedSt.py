aa7d2ff256539db76832a47e240c38bc58139694
enaudzius@g.hmc.edu <31321804+hmc-flea-f17@users.noreply.github.com>
2018-04-27 14:23:55 -0700

    Added up through vectors u and w

--- fishMotion1-variedSt.py
+++ fishMotion1-variedSt.py
@@ -21,6 +21,7 @@ dimLam = 0.95
 L = 1
 k = 2*math.pi/(dimLam*1)
 omega = 2*math.pi*0.3/(2*.01)
+omegaT = [0, .25*math.pi, .5*math.pi, .75*math.pi, math.pi, 1.25*math.pi, 1.5*math.pi, 1.75*math.pi, 2*math.pi]
 
 # Constants for the function a
 a0 = 0.02
@@ -80,7 +81,15 @@ def dHdZ(z, t):
     '''
     return (dAdZ(z)*math.sin(k*z-omega*t) +a(z)*k*math.cos(k*z-omega*t))
 
+def dHdt(z,t):
+    '''
+        Finds Borazjani's dh/dt (eqivalent to Lighthill dz/dt)
+    '''
+    return (-omega*a(z)*math.sin(k*z-omega*t))
 
+'''
+    These help set initial time and z variables
+'''
 times = [0, 0.1, 0.2, 0.3, 0.5, 1, 2, 4, 9, 10, 20]
 time = 1
 # Makes list with values 0-1 with 1/clarity increments
@@ -88,10 +97,15 @@ clarity = 100
 zVals = range(0,clarity)
 zVals = [z/clarity for z in zVals]
 
+'''
+    The following lines were used to recreate the weird Borzjani wavy graph.
+    All variables are in Borazjani coordinates
+'''
 # Makes list of u vals
+# Note: these U-vals refer to Borazjani's forward velocity
 numUVals = 6
-uVals = range(-numUVals,numUVals)
-uVals = [u/numUVals+.1 for u in uVals]
+UVals = range(-numUVals,numUVals)
+UVals = [u/numUVals+.1 for u in UVals]
 
 # Use list comprehension to get values of h
 # The list comprehension seems to work, but I keep getting the same few values for multiple values of time
@@ -99,9 +113,15 @@ hVals = [[h(z, t) for z in zVals] for t in times]
 
 
 # Try to use h2 function to vary omega
-h2Vals = [[h2(z, time, omega1(u), k1(u)) for z in zVals] for u in uVals]
+h2Vals = [[h2(z, time, omega1(u), k1(u)) for z in zVals] for u in UVals]
 aVals = [a(z) for z in zVals]
 
+
+'''
+    Here, we tried to put Borazjani into Lighthill 
+    I labeled Borzajani-based coordinates with a b afterwards, and 
+    Lighthill-based coordinates with an l afterwards.
+'''
 # Determine (dh/dz)^2, then calculate the derivative of the arclength
 dhdz2Vals = [dHdZ(z,time)**2 for z in zVals]
 sPrime = [math.sqrt(1+dHdZ(z,time)**2) for z in zVals]
@@ -109,32 +129,78 @@ sPrime = [math.sqrt(1+dHdZ(z,time)**2) for z in zVals]
 dz = L/clarity
 print(dz)
 sRects = [sP*dz for sP in sPrime]
-arcLen = [sum(sRects[0:z]) for z in range(0,len(sRects))]
+
+# The following list comp produces a nearly linear fit...
+arcLen = [sum(sRects[0:z]) for z in range(0,len(zVals))]
 
 def estArc(z):
     '''
-        Calculates the estimated arc length 
+        Calculates the estimated arc length
+        Note: doesn't work because z is not actually an index
+        Just use the arcLen list comp above
     '''
     return sum(sRects[0:z*clarity])
 
+# Let's try to findd the arclen using the above function
+print (estArc(L))
 
+# Find arclen at various times
+def arcBasedOnTime(t):
+    # Set up all the variables as before but name them differently
+    # to avoid confusing Python
+    clar = 100
+    zV = range(0,clarity)
+    zV = [z/clarity for z in zV]
 
-plt.plot(zVals, arcLen)
-plt.show()
+    dhdz2 = [dHdZ(z,t)**2 for z in zV]
+    sPrime = [math.sqrt(1+dHdZ(z,t)**2) for z in zV]
 
-# plt.plot(zVals,hVals[1],zVals,hVals[3])
-# print(len(hVals[0]))
-# plt.plot(zVals,hVals[1])
-# print(len(hVals[1]))
-# plt.plot(zVals,hVals[2])
-# print(len(hVals[2]))
-# plt.plot(zVals,hVals[3])
-# print(len(hVals[3]))
+    dzb = L/clar
+    sRects = [sP*dzb for sP in sPrime]
+
+    # We want to find the arclength across the whole fish, so add everything
+    return sum(sRects)
+
+# Now make a list based off of times 
+# note: omega is ~94. We need to vary omega*t between 0 and 2*pi.
+# We find that 2*pi/94 = 0.66, so varying times between 0 and 0.66
+# represents varied times over a period.
+timesForArc = [0.66*t/clarity for t in range(0,100)]
+arcsBasedtimes = [arcBasedOnTime(t) for t in timesForArc]
+# All of the arclengths are about 1, which is what we should hope
+
 
 '''
+    General Notes from meetings bcs wow good organization:
+
     Finish find a-coord (arcLen)
     Xl = Zb
-    Lighthill's dx/da = sqrt(1+dhdz^2)
+    Lighthill's dx/da = 1/sqrt(1+dhdz^2) = 1/sPrime
+    dxl/dt is the forward velocity of the body (Use body len/sec = 1)
+    dzl/dt is dh/dt
+    To find dzl/da, we should find (dhb/dzb)/(dab/dzb)
 '''
+# Let's find dx/da and dz/da (Lighthill vars)
+# Note: (dab/dzb) = da/dxl = sPrime
+dxda = [1/sP for sP in sPrime]
+dzda = [dHdZ(z,time)/math.sqrt(1+dHdZ(z,time)**2) for z in zVals]
 
-# Show the plot
+# Test whether (dx/da)^2 + (dz/da)^2 = 1
+tests = [dxa**2+dza**2 for (dxa,dza) in zip(dxda,dzda)]
+# If you print tests, you'll see that the proposition is correct.
+# Therefore, we can use the model
+
+# Now, let's try to find the u and w vectors from Lighthill
+# u = dx/dt*dx/da + dz/dt*dz/da
+dzdt = [dHdt(z,time) for z in zVals]
+uVec = [1*dxa + dzt*dza for (dxa,dzt,dza) in zip(dxda,dzdt,dzda)]
+# w = dz/dt*dx/da - dx/dt*dz/da
+wVec = [dzt*dxa - 1*dza for (dxa,dzt,dza) in zip(dxda,dzdt,dzda)]
+
+#plt.plot(zVals, dzda)
+plt.show()
+
+# Next, we need to find momentum...
+
+
+# Now let's try to find dzl/dal = dhb/dal
